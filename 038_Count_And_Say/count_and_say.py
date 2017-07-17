6a5bbc324ef30051d8f79a44e8546e6a623eb29d
FANG <513069595@qq.com>
2017-07-17 21:56:19 +0800

    计数和发言

---
+++ 038_Count_And_Say/count_and_say.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        s = '1'
+        for i in range(n-1):
+            temp = []
+            count = 1
+            for index in range(1,len(s)):
+                if s[index-1] == s[index]:
+                    count += 1
+                else:
+                    temp.append(str(count))
+                    temp.append(s[index-1])
+                    count = 1
+            temp.append(str(count))
+            temp.append(s[-1])
+            s = ''.join(temp)
+        return s
