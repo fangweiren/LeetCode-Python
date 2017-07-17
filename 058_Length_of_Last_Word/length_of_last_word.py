66dd6be2b861549c03e1fa8b919df2d56c36e640
FANG <513069595@qq.com>
2017-07-17 23:20:53 +0800

    最后一个单词的长度

---
+++ 058_Length_of_Last_Word/length_of_last_word.py
@@ -0,0 +1,7 @@
+ass Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return 0 if len(s.split()) == 0 else len(s.split()[-1])
