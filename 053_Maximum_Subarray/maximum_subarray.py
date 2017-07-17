2efd878ceb369722d8deb52fd29fc9712a5a7378
FANG <513069595@qq.com>
2017-07-17 23:01:07 +0800

    找出一个数组中总和最大的连续子数组（至少含有一个数）

---
+++ 053_Maximum_Subarray/maximum_subarray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        curSum = maxSum = nums[0]
+        for num in nums[1:]:
+            curSum = max(num, curSum + num)
+            maxSum = max(maxSum, curSum)
+											            
+        return maxSum
