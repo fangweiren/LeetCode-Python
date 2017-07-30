c60e5382c409eb39f9f005af6aa6f9b8c0e50dc1
FANG <513069595@qq.com>
2017-07-31 00:32:25 +0800

    给定每天的股票价格，如果只允许进行一轮交易，也就是买进一次和卖出一次，求所能获得的最大的利润。

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock/best_time_to_buy_sell_stock.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) < 2:
+            return 0
+        min_price = prices[0]
+        max_profit = 0
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            if price - min_price > max_profit:
+                max_profit = price - min_price
+        return max_profit
