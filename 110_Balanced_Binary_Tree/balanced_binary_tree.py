f56d5b95d254ad592ae7ccde300b198c055c40ec
FANG <513069595@qq.com>
2017-07-30 18:02:18 +0800

    平衡二叉树

---
+++ 110_Balanced_Binary_Tree/balanced_binary_tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def height(self, root):
+        if not root:
+            return 0
+        return max(self.height(root.left), self.height(root.right)) + 1
+					    
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        if abs(self.height(root.left) - self.height(root.right)) <= 1:
+            return self.isBalanced(root.left) and self.isBalanced(root.right)
+        else:
+            return False
