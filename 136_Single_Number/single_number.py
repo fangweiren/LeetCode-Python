9f8629fa8c87bd417f436682e97c685be6e53d8e
FANG <513069595@qq.com>
2017-07-31 11:05:57 +0800

    一个数组中除了一个数字出现过一次外，其余的数字都出现了两次，找出那个只出现一次的数字

---
+++ 136_Single_Number/single_number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = nums[0]
+        for num in nums[1:]:
+            res ^= num
+        return res
